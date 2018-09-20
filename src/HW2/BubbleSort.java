92a2f642c8d090f78ebb2defa34dede990fa1d36
namesconnor <connormorrison@montana.edu>
2018-09-20 09:36:03 -0600

    Updated
    Connor

--- src/HW2/BubbleSort.java
+++ src/HW2/BubbleSort.java
@@ -2,8 +2,18 @@ package HW2;
 
 public class BubbleSort implements SortBehavior {
     @Override
-    public int[] mathSort() {
-        System.out.println("Bubble sort");
-        return new int[0];
+    public int[] mathSort(int[] array) {
+    	System.out.println("\nBubble Sort");
+        int n = array.length;
+        for(int x = 0; x < n-1; x++){
+        	for(int y = 0; y < n-x-1; y++){
+        		if(array[y] > array[y+1]){
+        			int saved = array[y];
+        			array[y] = array[y+1];
+        			array[y+1] = saved;
+        		}
+        	}
+        }
+        return array;
     }
 }
