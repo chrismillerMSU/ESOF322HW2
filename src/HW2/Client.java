a0f3263719ed13d81aaca2553f13ed5a20be1af9
namesconnor <connormorrison@montana.edu>
2018-09-20 09:35:31 -0600

    Updated
    Connor

--- src/HW2/Client.java
+++ src/HW2/Client.java
@@ -8,14 +8,16 @@ public class Client {
         int[] array = new int[]{7,4,9,2,0,12,15};
         mathematica.performSort(array);
         printArray(array);
-       // mTool.performSort();
+        myMath.performSort(array);
+        printArray(array);
+        // mTool.performSort();
        // myMath.performSort();
        // myMath.setSortStrategy(new MergeSort());
        // myMath.performSort();
     }
     public static void printArray(int[] array){
         for(int i: array){
-            System.out.println(i);
+            System.out.print(i + ",");
         }
     }
 }
